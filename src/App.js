a35058cd5cae9a453fb8f53d6fb1ca3b5522468a
eddy <eddyhernandez0921@live.com>
2018-12-01 01:10:13 -0600

    added dwwrapper and styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { WithStoreAndRouter, WrapperActions } from "dw-app-wrapper";
+import "./App.css";
+class Home extends Component {
+  render() {
+    console.log(this);
+    return <div>test</div>;
+  }
+}
+const ConnectedHome = connect(
+  state => state,
+  dispatch => ({})
+)(Home);
+const ConnectedApp = WithStoreAndRouter();
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ConnectedApp hasDrawer={true}>
+        <ConnectedHome />
+      </ConnectedApp>
     );
   }
 }
